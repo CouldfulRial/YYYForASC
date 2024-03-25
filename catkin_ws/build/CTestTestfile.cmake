cb2d4706de3ceaa50ede6ba57dd0ce6076b870c2
CouldfulRial <yfgcould@myyahoo.com>
2024-03-25 19:29:13 +1100

    Removed build and devel folders

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/couldubuntu/YYYForASC/catkin_ws/src
-# Build directory: /home/couldubuntu/YYYForASC/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("navigation")
