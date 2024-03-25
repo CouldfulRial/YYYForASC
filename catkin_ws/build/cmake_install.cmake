766379bd2b1f4a7c941793d08d7eaf874f0a49a9
CouldfulRial <yfgcould@myyahoo.com>
2024-03-25 18:41:05 +1100

    Created a new package for navigation

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/couldubuntu/YYYForASC/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/couldubuntu/YYYForASC/catkin_ws/build/navigation/cmake_install.cmake")
 
 endif()
 
