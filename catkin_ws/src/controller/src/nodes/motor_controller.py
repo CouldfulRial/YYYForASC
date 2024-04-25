9ed0b0b7416f7b471e91d64b96240ab60469c529
CouldfulRial <yfgcould@myyahoo.com>
2024-04-26 09:31:44 +1000

    Motor controller modify, ready for testing

--- catkin_ws/src/controller/src/nodes/motor_controller.py
+++ catkin_ws/src/controller/src/nodes/motor_controller.py
@@ -1,41 +1,41 @@
 #! /usr/bin/env python
-
 '''
 This node controls the motors (inner controller)
 Subscribed topics:
-    measured_vel              [geometry_msgs/Twist]
-    cmd_vel                   [geometry_msgs/Twist]
+    mes_speeds                [asclinic_pkg/LeftRightFloat32]
+    cmd_speeds                [asclinic_pkg/LeftRightFloat32]
 Published topics:
     /asc/set_motor_duty_cycle [asclinic_pkg/LeftRightFloat32]
 '''
 
 import rospy
+import rospkg
+import datetime
+import csv
 from geometry_msgs.msg import Twist
 from asclinic_pkg.msg import LeftRightFloat32, LeftRightFloat32
 import matplotlib.pyplot as plt
 
 # Constatns
-WHEEL_DIAMETER = 0.144  # m
-WHEEL_RADIUS = WHEEL_DIAMETER / 2
-WHEEL_BASE = 0.218  # m
-HALF_WHEEL_BASE = WHEEL_BASE / 2
+TIME_STEP = 0.1  # s
 
 class MotorController:
     def __init__(self):
-         # Initialise node
-        rospy.init_node('motor_controller')
+        # Initialise node
+        self.node_name = 'motor_controller'
+        rospy.init_node(self.node_name)
 
         # Get user parameter
-        self.parm  = rospy.get_param('motor_controller')
+        self.parm  = rospy.get_param(self.node_name)
         self.verbosity = self.parm["verbosity"]
-        self.plot      = self.parm["plot"]
+        self.save_data = self.parm["save_data"]
 
         # Subscribers
-        self.ref_speed_sub = rospy.Subscriber('cmd_vel', Twist, self.ref_twist_callback)
-        self.mes_speed_sub = rospy.Subscriber('measured_vel', Twist, self.mes_twist_callback)
+        self.ref_speed_sub = rospy.Subscriber('cmd_speeds', Twist, self.ref_speeds_callback)
+        self.mes_speed_sub = rospy.Subscriber('mes_speeds', Twist, self.mes_speeds_callback)
 
-        # Timer: Calls the timer_callback function at 20 Hz
-        self.timer = rospy.Timer(rospy.Duration(0.05), self.timer_callback)
+        # Timer: Calls the timer_callback function at 10 Hz
+        self.timer = rospy.Timer(rospy.Duration(TIME_STEP), self.timer_callback)
 
         # Publisher
         self.duty_cycle_pub = rospy.Publisher('/asc/set_motor_duty_cycle', LeftRightFloat32, queue_size=10)
@@ -43,31 +43,38 @@ class MotorController:
         # Register the shutdown callback
         rospy.on_shutdown(self.shutdown_callback)
         if self.plot == 1:
-            self.left_speed_list = []
-            self.right_speed_list = []
-            self.desired_left_speed_list = []
-            self.desired_right_speed_list = []
+            self.time_list = [0]
+            self.left_speed_list = [0]
+            self.right_speed_list = [0]
+            self.desired_left_speed_list = [0]
+            self.desired_right_speed_list = [0]
 
-        # Initialise speeds variables
+        # Initialise variables
+        # left
         self.current_left_speed = 0.0
-        self.current_right_speed = 0.0
         self.desired_left_speed = 0.0
-        self.desired_right_speed = 0.0
-
-        # Integral and Derivative components
         self.integral_left = 0.0
         self.integral_right = 0.0
+        # right
+        self.current_right_speed = 0.0
+        self.desired_right_speed = 0.0
         self.last_error_left = 0.0
         self.last_error_right = 0.0
 
+        # Get save path. Set the file name to the intial time
+        self.save_path = self.get_save_path()
+        intial_time = datetime.datetime.now()
+        self.formatted_time = intial_time.strftime('%y%m%d_%H_%M_%S')
+
     def timer_callback(self, event):
-        # Controller execution callback
+        # Controller algorithm
         # Error calculation
         error_left = self.desired_left_speed - self.current_left_speed
         error_right = self.desired_right_speed - self.current_right_speed
 
-        # Store the speeds
+        # Store the speeds data
         if self.plot == 1:
+            self.time_list.append(self.time_list[-1] + TIME_STEP)
             self.left_speed_list.append(self.current_left_speed)
             self.right_speed_list.append(self.current_right_speed)
             self.desired_left_speed_list.append(self.desired_left_speed)
@@ -75,25 +82,24 @@ class MotorController:
 
         duty_cycle_left, duty_cycle_right = self.controller(error_left, error_right)
 
-        # Publish duty cycles
-        self.duty_cycle_pub.publish(LeftRightFloat32(left=duty_cycle_left, right=duty_cycle_right))
-
-        self.last_error_left = error_left
-        self.last_error_right = error_right
-
         # Diaply in the console
         if self.verbosity == 1:
             rospy.loginfo("-"*25 + "Motor Controller" + "-"*25 +  
                         f"\nDesired Left Speed: {self.desired_left_speed:3.2f}, Desired Right Speed: {self.desired_right_speed:3.2f}\n" + 
                         f"Current Left Speed:   {self.current_left_speed:3.2f}, Current Right Speed: {self.current_right_speed:3.2f}\n" + 
                         f"Error_left:           {error_left:3.2f},              Error_right:         {error_right:3.2f}\n" + 
-                        f"Current Left Speed:   {self.current_left_speed:3.2f}, Current Right Speed: {self.current_right_speed:3.2f}\n" + 
                         f"Left Duty Cycle:      {duty_cycle_left:3.2f},         Right Duty Cycle:    {duty_cycle_right:3.2f}\n")
+            
+        # Publish duty cycles
+        self.duty_cycle_pub.publish(LeftRightFloat32(left=duty_cycle_left, right=duty_cycle_right))
+
+        self.last_error_left = error_left
+        self.last_error_right = error_right
 
     def controller(self, error_left, error_right):
         # PID parameters
-        Kp = 20
-        Ki = 20
+        Kp = 2
+        Ki = 2
         Kd = 0
 
         # I
@@ -109,40 +115,65 @@ class MotorController:
 
         return duty_cycle_left, duty_cycle_right
 
-    def ref_twist_callback(self, data):
-        v = data.linear.x
-        omega = data.angular.z
+    def ref_speeds_callback(self, data):
+        self.desired_left_speed, self.desired_right_speed = data.left, data.right
 
-        self.desired_left_speed, self.desired_right_speed = self.twist_to_speeds(v, omega)
+    def mes_speeds_callback(self, data):
+        self.current_left_speed, self.current_right_speed = data.left, data.right
 
-    def mes_twist_callback(self, data):
-        v = data.linear.x
-        omega = data.angular.z
 
-        self.current_left_speed, self.current_right_speed = self.twist_to_speeds(v, omega)
-
-    @staticmethod
-    def twist_to_speeds(v, omega):
-        delta_theta_l = (v - omega * HALF_WHEEL_BASE) / WHEEL_RADIUS
-        delta_theta_r = (v + omega * HALF_WHEEL_BASE) / WHEEL_RADIUS
-
-        return delta_theta_l, delta_theta_r
-
+    ##############################################################################################################
+    ############################## The following codes are not relevant to algorithm ##############################
+    ##############################################################################################################
     def shutdown_callback(self):
-        rospy.loginfo("Shutting Down Motors....")
-        self.duty_cycle_pub.publish(LeftRightFloat32(left=0, right=0))
-        if self.plot == 1:
-            self.plot_callback()
+        rospy.loginfo(f"Shutting down node {self.node_name}....")
+        while self.current_left_speed != 0 or self.current_right_speed != 0:
+            self.duty_cycle_pub.publish(LeftRightFloat32(left=0, right=0))
+        if self.save_data == 1:
+            rospy.loginfo("Saving Data....")
+            self.plot()
+            self.data()
+            rospy.loginfo("Saving Data Completed....")
 
-    def plot_callback(self):
-        plt.plot(self.left_speed_list, label='Left Wheel Speed')
-        plt.plot(self.right_speed_list, label='Right Wheel Speed')
-        plt.plot(self.desired_left_speed_list, label='desired left')
-        plt.plot(self.desired_right_speed_list, label='desired right')
-        plt.xlabel('steps')
-        plt.ylabel('speeds')
-        plt.title("motor controller")
-        plt.show()
+    def plot(self):
+        # set the font size
+        plt.rcParams.update({'font.size': 5})
+
+        # plot left
+        ax1 = plt.subplot(2, 1, 1)
+        plt.plot(self.time_list, self.left_speed_list, label='Left Wheel Speed')
+        plt.plot(self.time_list, self.desired_left_speed_list, label='desired left')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Left Wheel Speed (rad/s)')
+        plt.title("Motor Controller")
+        plt.legend()
+        ax1.grid(True)
+        
+        # plot right
+        ax2 = plt.subplot(2, 1, 2)
+        plt.plot(self.time_list, self.right_speed_list, label='Right Wheel Speed')
+        plt.plot(self.time_list, self.desired_right_speed_list, label='desired right')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Right Wheel Speed (rad/s)')
+        plt.legend()
+        ax2.grid(True)
+
+        # Optionally adjust layout
+        plt.tight_layout()
+
+        # Save the figure
+        plt.savefig(f'{self.save_path}motor_controller_{self.formatted_time}.png', format='png', dpi=300)
+
+    def data(self):
+        with open(f'{self.save_path}motor_controller_{self.formatted_time}.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Time (s)', 'Left Wheel Speed (rad/s)', 'Right Wheel Speed (rad/s)', 'Desired Left Speed (rad/s)', 'Desired Right Speed (rad/s)'])
+            for i in range(len(self.time_list)):
+                writer.writerow([self.time_list[i], self.left_speed_list[i], self.right_speed_list[i], self.desired_left_speed_list[i], self.desired_right_speed_list[i]])
+
+    def get_save_path(self):
+        rospack = rospkg.RosPack()
+        return rospack.get_path(self.node_name) + 'src/data/'
 
 if __name__ == '__main__':
     controller = MotorController()
--- catkin_ws/src/controller/src/nodes/motor_model.py
+++ catkin_ws/src/controller/src/nodes/motor_model.py
@@ -6,7 +6,7 @@ Subscribed topics:
     /asc/current_motor_duty_cycle  [asclinc_pkg/LeftRightFloat32]
     /asc/encoder_counts            [asclinc_pkg/LeftRightInt32]
 Published topics:
-    /measured_vel                  [geometry_msgs/Twist]
+    /mes_speeds                 [asclinic_pkg/LeftRightFloat32]
 '''
 from math import pi
 import rospy
@@ -16,10 +16,6 @@ from asclinic_pkg.msg import LeftRightFloat32, LeftRightInt32
 from geometry_msgs.msg import Twist, Vector3
 
 # Constants
-WHEEL_DIAMETER = 0.144  # m
-WHEEL_RADIUS = WHEEL_DIAMETER / 2
-WHEEL_BASE = 0.218  # m
-HALF_WHEEL_BASE = WHEEL_BASE / 2
 CPR = 1120
 RADIAN_PER_COUNT = 2 * pi / CPR
 
@@ -28,45 +24,45 @@ TIME_STEP = 0.1  # s, which is the interval encoder publishes data
 
 class MotorModel:
     def __init__(self):
-        # Get user parameter
-        self.verbosity = rospy.get_param('~verbosity', 1)
-
         # Initialise node
-        rospy.init_node('motor_model', anonymous=True)
+        self.node_name = 'motor_model'
+        rospy.init_node(self.node_name, anonymous=True)
+
+        # Get user parameters
+        self.parms = rospy.get_param(self.node_name)
+        self.verbosity = self.parms['verbosity']
 
         # Initialise the subscribers
         self.sub_encoder = rospy.Subscriber('/asc/encoder_counts', LeftRightInt32, self.update_encoder_callback)
         self.sub_dir = rospy.Subscriber('/asc/current_motor_duty_cycle', LeftRightFloat32, self.update_dir_callback)
 
-        # Initialise the publisher and tf broadcaster
-        self.twist_pub = rospy.Publisher("measured_vel", Twist, queue_size=10)
+        # Initialise the publisher
+        self.speeds_pub = rospy.Publisher("mes_speeds", LeftRightFloat32, queue_size=10)
 
         # Initialise the variables
         self.left_dir, self.right_dir = 1, 1  # 1 for forward, -1 for backward
         self.delta_theta_l, self.delta_theta_r = 0, 0
         self.seq = 0
 
-
     def update_encoder_callback(self, data):
         # Update the encoder counts to delta theta for both wheels in RADIAN
-        self.delta_theta_l = self.left_dir * data.left * RADIAN_PER_COUNT
-        self.delta_theta_r = self.right_dir * data.right * RADIAN_PER_COUNT
+        self.delta_theta_l = self.left_dir * data.left * RADIAN_PER_COUNT / TIME_STEP  # rad/s
+        self.delta_theta_r = self.right_dir * data.right * RADIAN_PER_COUNT / TIME_STEP  # rad/s
         self.seq = data.seq_num
-
-        # Convert the delta theta to v and omega
-        v     = WHEEL_RADIUS * (self.delta_theta_l + self.delta_theta_r) / 2           / TIME_STEP # m/s
-        omega = WHEEL_RADIUS * (self.delta_theta_r - self.delta_theta_l) / WHEEL_BASE  / TIME_STEP # rad/s
 
         # log info
         if self.verbosity == 1:
             rospy.loginfo("-"*25 + "Motor Model" + "-"*25 + 
-                          f"\nseq: {self.seq}, v: {v:3.5f}, omega: {omega:3.5f}")
+                          f"\nseq: {self.seq}:" + 
+                          f"\n\tleft wheel speed: {self.delta_theta_l} rad/s" +
+                          f"\n\tright wheel speed: {self.delta_theta_r} rad/s")
 
         # Publish the speeds
-        self.twist_pub.publish(
-            Twist(
-                linear =Vector3(x=v, y=0, z=0),
-                angular=Vector3(x=0, y=0, z=omega)
+        self.speeds_pub.publish(
+            LeftRightFloat32(
+                seq_num=self.seq,
+                left=self.delta_theta_l,
+                right=self.delta_theta_r
                   ))
 
     def update_dir_callback(self, data):
