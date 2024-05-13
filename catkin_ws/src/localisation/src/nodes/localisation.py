d44dfdbea678fd9e40e69bb2b197d0a86a608868
CouldfulRial <yfgcould@myyahoo.com>
2024-05-13 22:02:32 +1000

    kalman filter update

--- catkin_ws/src/localisation/src/nodes/localisation.py
+++ catkin_ws/src/localisation/src/nodes/localisation.py
@@ -57,6 +57,7 @@ class Localisation:
         self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
 
         # Initialise parameters
+        self.delta_theta_l, self.delta_theta_r = 0.0, 0.0
         self.Delta_s_t   = 0.0
         self.Delta_psi_t = 0.0
         self.z_t_1 = np.array([0.0, 0.0, 0.0])  # pose estimate from computer vision at time t-1
@@ -64,41 +65,58 @@ class Localisation:
         self.phat_t_given_t = np.array([0.0, 0.0, 0.0])  # pose estimate after fusion of odometry with CV update t
         self.phat_t_given_t_1 = np.array([0.0, 0.0, 0.0])  # pose estimate from odometry at time t given the previous CV update t-1
         self.phat_t_1_given_t_1 = np.array([0.0, 0.0, 0.0])  # pose estimate from odometry at time t-1 given the previous CV update t-1
+        self.Kt = np.zeros((3, 3))  # Kalman gain
 
         # Covariance matrices
         self.pose_cov_t_given_t = np.zeros((3, 3))  # Covariance of the pose estimate at time t given t
         self.pose_cov_t_given_t_1 = np.zeros((3, 3))  # Covariance of the pose estimate at time t given t-1
-        self.pose_cov_t_1_given_t_1 = np.zeros((3, 3))  # Covariance of the pose estimate at time t-1 given t-1
+        self.pose_cov_t_1_given_t_1 = np.eye(3) * 0.01 # Covariance of the pose estimate at time t-1 given t-1
         # Update the following covariance matrix after measurements
         self.cv_cov_t = np.eye(3) * 0.01  # Covariance of the computer vision estimate at time t
 
         # vodom failure flag
-        self.vodom_failure = True
+        self.vodom_failure = False
 
         # Timer: Calls the timer_callback function at frequency in Hz
         # The vodom will be published at 10Hz
         self.current_time = rospy.Time.now()
-        self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
+        self.timer = rospy.Timer(rospy.Duration(0.2), self.timer_callback)
 
     def wodom_callback(self, data:LeftRightFloat32):
         # Implement lecture L07 P10
         # Extract the updated change in both wheels at t
         # Update the pose estimate at t given the previous vodom z_t-1
-        delta_theta_l = data.left
-        delta_theta_r = data.right
+        self.delta_theta_l = data.left
+        self.delta_theta_r = data.right
 
-        # Map to Delta_s, Delta_psi
-        self.Delta_s_t   = WHEEL_RADIUS / 2          * (delta_theta_l + delta_theta_r)
-        self.Delta_psi_t = WHEEL_RADIUS / WHEEL_BASE * (delta_theta_r - delta_theta_l)
+        self.Delta_s_t   = WHEEL_RADIUS / 2          * (self.delta_theta_l + self.delta_theta_r)
+        self.Delta_psi_t = WHEEL_RADIUS / WHEEL_BASE * (self.delta_theta_r - self.delta_theta_l)
+
+        ## Update Steps
+        # Update the predicted pose according to f_update
         inside = self.z_t_1[PSI] + self.Delta_psi_t / 2
-
-        # Update the predicted pose according to f_up
         self.phat_t_given_t_1[X]   = self.phat_t_1_given_t_1[X]   + self.Delta_s_t * np.cos(inside)
         self.phat_t_given_t_1[Y]   = self.phat_t_1_given_t_1[Y]   + self.Delta_s_t * np.sin(inside)
         self.phat_t_given_t_1[PSI] = self.phat_t_1_given_t_1[PSI] + self.Delta_psi_t
 
         # Update the predicted covariance
-        self.update_pose_cov(delta_theta_l, delta_theta_r, inside, self.Delta_s_t)
+        self.update_pose_cov(self.delta_theta_l, self.delta_theta_r, inside, self.Delta_s_t)
+
+    def vodom_callback(self, data:Odometry):
+        # Based on the vision-based localisation
+        # Extract the current pose z_t
+        x = data.pose.pose.position.x
+        y = data.pose.pose.position.y
+        psi = self.quat_to_euler(data.pose.pose.orientation)
+
+        # Update the current pose z_t
+        self.z_t = np.array([x, y, psi])
+
+    def vfail_callback(self, data:Bool):
+        if data.data:
+            self.vodom_failure = True
+        else:
+            self.vodom_failure = False
 
     def update_pose_cov(self, delta_theta_l, delta_theta_r, inside, Delta_s_t):
         # This function implements lecture L05 P17 and P33
@@ -109,61 +127,47 @@ class Localisation:
 
         # Put the covariance of the pose into the return variable
         # Linearisation w.r.t. states
-        pose_jacobians = [  # 3x3
+        pose_jacobians = np.array([  # 3x3
             [1, 0, -Delta_s_t * np.sin(inside)],
             [0, 1,  Delta_s_t * np.cos(inside)],
             [0, 0,  1]
-        ]
+        ])
 
         # Linearisation w.r.t. inputs
         r = WHEEL_RADIUS
         b = HALF_WHEEL_BASE
-        theta_jacobians = [  # 3x2
+        theta_jacobians = np.array([  # 3x2
             [ r/2 * np.cos(inside) + r/(4*b) * Delta_s_t * np.sin(inside), r/2 * np.cos(inside) - r/(4*b) * Delta_s_t * np.sin(inside)],
             [ r/2 * np.sin(inside) - r/(4*b) * Delta_s_t * np.cos(inside), r/2 * np.sin(inside) + r/(4*b) * Delta_s_t * np.cos(inside)],
             [-r / (2*b),                                                   r / (2*b)]
-        ]
+        ])
 
         # Update the pose covariance matrix (3x3)
-        if not self.vodom_failure:
-            # If the vision-based localisation is working
-            self.pose_cov_t_given_t_1 = pose_jacobians @ self.pose_cov_t_1_given_t_1 @ np.transpose(pose_jacobians) + theta_jacobians @ theta_covariance @ np.transpose(theta_jacobians)
-        else:
-            # If the vision-based localisation fails, then we assume wodom is the only source of information
-            self.pose_cov_t_given_t_1 = np.zeros((3, 3))
-
-    def vodom_callback(self, data:Odometry):
-        # Based on the vision-based localisation
-        # Extract the current pose z_t
-        x = data.pose.pose.position.x
-        y = data.pose.pose.position.y
-        psi = self.quat_to_euler(data.pose.pose.orientation)
-
-        # Update the current pose z_t, and z_t-1
-        self.z_t_1 = self.z_t
-        self.z_t = np.array([x, y, psi])
-
-    def vfail_callback(self, data:Bool):
-        if data.data:
-            self.vodom_failure = True
-        else:
-            self.vodom_failure = False
+        self.pose_cov_t_given_t_1 = pose_jacobians  @ self.pose_cov_t_1_given_t_1 @ np.transpose(pose_jacobians) + \
+                                    theta_jacobians @ theta_covariance            @ np.transpose(theta_jacobians)
 
     def timer_callback(self, event):
+        ## Update Step
         # Update Kalman gain Kt
-        Kt = self.pose_cov_t_given_t_1 @ np.linalg.inv(self.pose_cov_t_given_t_1 + self.cv_cov_t)
+        self.Kt = self.pose_cov_t_given_t_1 @ np.linalg.inv(self.pose_cov_t_given_t_1 + self.cv_cov_t)
 
         # Update the pose estimate
-        self.phat_t_1_given_t_1 = self.phat_t_given_t
-        self.phat_t_given_t = self.phat_t_given_t_1 + Kt @ (self.z_t - self.phat_t_given_t_1)
+        self.phat_t_given_t = self.phat_t_given_t_1 + self.Kt @ (self.z_t - self.phat_t_given_t_1)
 
         # Update the covariance matrix
         self.pose_cov_t_1_given_t_1 = self.pose_cov_t_given_t
-        self.pose_cov_t_given_t = self.pose_cov_t_given_t_1 - Kt @ (self.pose_cov_t_given_t_1 + self.cv_cov_t) @ np.transpose(Kt)
+        if not self.vodom_failure:
+            self.pose_cov_t_given_t = self.pose_cov_t_given_t_1 - self.Kt @ (self.pose_cov_t_given_t_1 + self.cv_cov_t) @ np.transpose(self.Kt)
+        else:
+            self.pose_cov_t_given_t = self.pose_cov_t_given_t_1
 
         # Publish the odom at a fixed rate
         self.current_time = rospy.Time.now()
         self.publish_odom()
+
+        # Step forward
+        self.phat_t_1_given_t_1 = self.phat_t_given_t
+        self.z_t_1 = self.z_t
 
     def publish_odom(self):
         # Create the odometry message
@@ -179,7 +183,13 @@ class Localisation:
 
         if self.verbosity == 1:
             rospy.loginfo("-"*25 + "Localisation" + "-"*25 +
-                          f"\n vodom fails: {self.vodom_failure}" +
+                         f"\n camera fails: {self.vodom_failure}" +
+                          f"\n z_t: \n{self.z_t}" +
+                          f"\n prev pose: \n{self.phat_t_1_given_t_1}" +
+                          f"\n wodom predicted pose: \n{self.phat_t_given_t_1}" +
+                          f"\n predicted pose covariance: \n{self.pose_cov_t_given_t_1}" +
+                          f"\n kalman gain: \n{self.Kt}" +
+                          f"\n updated pose: \n{self.phat_t_given_t}" +
                           f"\n x: {x:3.5f}, y: {y:3.5f}, psi: {psi/np.pi:3.5f}pi" + 
                           f"\n pose covariance: \n{self.pose_cov_t_given_t}")
 
